949888f4d0d8bc1d0ef2f2d7c2c0410b4d0d1179
Angel Pauly <angelpauly72@gmail.com>
2026-02-05 01:12:06 +0530

    modified upload and analyze routes

--- backend/app/routes/upload.py
+++ backend/app/routes/upload.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, UploadFile, File
 from app.config import UPLOAD_DIR
 import os
 
-router = APIRouter()
+router = APIRouter(prefix="/upload", tags=["Upload"])
 
 @router.post("/")
 async def upload_report(file: UploadFile = File(...)):
     os.makedirs(UPLOAD_DIR, exist_ok=True)
+
     file_path = os.path.join(UPLOAD_DIR, file.filename)
 
     with open(file_path, "wb") as f:
@@ -14,8 +15,10 @@ async def upload_report(file: UploadFile = File(...)):
 
     return {
         "file_id": file.filename,
+        "file_path": file_path,
         "status": "uploaded"
     }
+
 @router.get("/")
 def test_upload():
     return {"message": "Upload route works"}
